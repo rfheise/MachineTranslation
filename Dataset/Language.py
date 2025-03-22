trying to fix vanishing gradient

Dataset/Language.py
@@ -18,7 +18,7 @@ device = Logger.device
 class Embeddings():
 
     special_toks = ["<PAD>","<SOS>", "<EOS>", "<UNK>"]
-    special_toks_val = {tok: ix for ix, tok in enumerate(special_toks)}
+    special_toks_val = None
     embedding_dim = 300
     pattern = regex.compile(r'\p{L}+|\d|[^\w\s]')
 
@@ -49,6 +49,7 @@ class Embeddings():
         self.word_to_tok = {word: ix for ix, word in enumerate(self.vocab)}
         self.tok_to_word = {ix: word for ix, word in enumerate(self.vocab)}
         self.embeddings = self.create_embeddings()
+        Embeddings.special_toks_val = {tok: ix for ix, tok in enumerate(Embeddings.special_toks)}
 
     def create_embeddings(self):
         embeddings = torch.randn((len(self.vocab), 300))
@@ -65,6 +66,13 @@ class Embeddings():
             return self.word_to_tok["<UNK>"]
         
         return self.word_to_tok[word]
+
+    def convert_row_to_tensor(self, pattern, row):
+        toks = pattern.findall(row)
+        toks = ["<SOS>",*toks, "<EOS>"]
+        toks = [int(self.get_token(tok)).to_bytes(4, byteorder='little') for tok in toks]
+        tok_bytes = b''.join(toks)
+        return tok_bytes
     
     def get_word(self, tok):
         return self.tok_to_word[int(tok)]
@@ -84,12 +92,21 @@ class BytePairEmbeddings(Embeddings):
             self.generate_byte_pair_encoding()
         self.tokenizer = spm.SentencePieceProcessor()
         self.tokenizer.load(tok_file)
+        Embeddings.special_toks_val = {ix:int(self.get_token(ix)) for ix in Embeddings.special_toks}
 
     def get_token_count(self):
         return self.tokenizer.get_piece_size()
     
     def get_embed_dim(self):
         return None
+    
+    def convert_row_to_tensor(self, pattern, row):
+        toks = row
+        # print(toks)
+        # print(self.sentence_to_tok(toks))
+        toks = [int(tok).to_bytes(4, byteorder='little') for tok in self.sentence_to_tok(toks)]
+        tok_bytes = b''.join(toks)
+        return tok_bytes
     
     def generate_byte_pair_encoding(self):
         dataset = "train"
@@ -114,7 +131,9 @@ class BytePairEmbeddings(Embeddings):
             model_type='bpe',              # Use Byte-Pair Encoding
             input_sentence_size=1000000,   # Use up to 1,000,000 sentences from the data
             shuffle_input_sentence=True,   # Shuffle sentences to reduce bias
-            user_defined_symbols=Embeddings.special_toks  # Additional symbols to include
+            user_defined_symbols=Embeddings.special_toks,  # Additional symbols to include
+            bos_piece = "<SOS>",
+            eos_piece = "<EOS>"
         )
         print("removing tmp output text file")
         os.remove(tmp_file)
@@ -127,13 +146,16 @@ class BytePairEmbeddings(Embeddings):
         return None
 
     def sentence_to_tok(self, sentence):
-        self.tokenizer.encode_as_pieces(sentence)
+        return [self.get_token(tok) for tok in self.tokenizer.encode_as_pieces(sentence)]
 
     def get_word(self, tok):
-        return self.tokenizer.decode_ids([tok])
+        return self.tokenizer.decode_ids([int(tok),])
 
     def get_token(self, word):
         return self.tokenizer.piece_to_id(word)
+    
+    def get_sentence(self, toks):
+        return self.tokenizer.decode_ids([int(tok) for tok in toks])
 
 
     
@@ -174,6 +196,10 @@ class Language(Dataset):
             lang = self.outlang 
         else:
             lang = self.inlang 
+        try:
+            return "".join(lang.get_sentence(sentence))
+        except:
+            pass
         toks = []
         for tok in sentence:
             word = lang.get_word(tok)
@@ -297,11 +323,8 @@ class LanguageData(Data):
 
     @staticmethod
     def convert_row_to_tensor(pattern, row, lang):
-        toks = pattern.findall(row)
-        toks = ["<SOS>",*toks, "<EOS>"]
-        toks = [int(lang.get_token(tok)).to_bytes(4, byteorder='little') for tok in toks]
-        tok_bytes = b''.join(toks)
-        return tok_bytes
+        return lang.convert_row_to_tensor(pattern, row)
+        
 
     def __len__(self):
         if self.conn is None:

Model/TransformerBoujee.py
@@ -11,7 +11,7 @@ from ..Search.Search import greedy_search, beam_search
 import random 
 from ..Log import Logger
 import collections
-
+import sys
 device = Logger.device
 
 
@@ -20,7 +20,7 @@ class Transformer(nn.Module):
     def __init__(self, embed_dim, num_tokens_in, num_tokens_out, single_embed=False):
         super().__init__()
 
-        self.transformer = nn.Transformer(d_model = embed_dim,batch_first=True, nhead=8, num_encoder_layers=6, num_decoder_layers=6)
+        self.transformer = nn.Transformer(d_model = embed_dim,batch_first=True, nhead=8, num_encoder_layers=6, num_decoder_layers=6, norm_first= True)
         self.linear = nn.Sequential(
             nn.Linear(embed_dim, embed_dim),
             nn.ReLU(),
@@ -36,13 +36,13 @@ class Transformer(nn.Module):
     
     def set_out_lang_embeddings(self, out_lang_embeddings):
         if not out_lang_embeddings:
-            print("Out Lang Embeddings Are Empty")
+            print("Warning: Out Lang Embeddings Are Empty", file=sys.stderr)
             return 
         self.out_lang_embeddings = nn.Embedding.from_pretrained(out_lang_embeddings, freeze=False).to(device)
     
     def set_in_lang_embeddings(self, in_lang_embeddings):
         if not in_lang_embeddings:
-            print("In Lang Embeddings Are Empty")
+            print("Warning: In Lang Embeddings Are Empty",file=sys.stderr)
             return 
         self.in_lang_embeddings = nn.Embedding.from_pretrained(in_lang_embeddings, freeze=False).to(device)
 
@@ -61,7 +61,7 @@ class Transformer(nn.Module):
 
 class TransformerBoujee(Model):
     
-    def __init__(self, lr, lr_decay, lr_step, batch_size):
+    def __init__(self, lr, lr_decay, lr_step, batch_size, total_epochs):
         super().__init__()
         self.lr = lr 
         self.lr_decay = lr_decay 
@@ -70,6 +70,7 @@ class TransformerBoujee(Model):
         self.transformer = None 
         self.use_scaler = (device == "cuda")
         self.embed_dim = 512
+        self.total_epochs = total_epochs
         if self.use_scaler:
             torch.backends.cudnn.benchmark = True
             torch.set_float32_matmul_precision('high')
@@ -80,6 +81,7 @@ class TransformerBoujee(Model):
         
         dataset.train_init()
         loader = get_language_loader(dataset.train, batch_size=self.batch_size, shuffle=True)
+        self.len_loader = len(loader)
         self.lazy_init(dataset)
         self.transformer.train()
         l_avg = Avg("Loss")
@@ -101,16 +103,19 @@ class TransformerBoujee(Model):
                 l = loss(out, target)
                 l.backward()
                 self.optim.step()
+                torch.nn.utils.clip_grad_norm_(self.transformer.parameters(), max_norm=1.0)
             else:
                 with torch.amp.autocast(device_type=device):
                     out = self.transformer(X,decoder_input, mask).transpose(1,2)
                     l = loss(out, target)
                 self.scaler.scale(l).backward()
+                self.scaler.unscale_(self.optim)
+                torch.nn.utils.clip_grad_norm_(self.transformer.parameters(), max_norm=1.0)
                 self.scaler.step(self.optim)
                 self.scaler.update()
             acc.compute_score(out.detach().argmax(dim=1), target.detach())
             l_avg.compute_score(l.detach())
-        self.scheduler.step() 
+            self.scheduler.step()    
 
         ref = dataset.decode_sentence(X[1].detach().cpu(), "inlang")
         out_sentence = dataset.decode_sentence(Y[1].detach().cpu())
@@ -151,7 +156,8 @@ class TransformerBoujee(Model):
             if self.use_scaler:
                 self.transformer = torch.compile(self.transformer)
             self.optim = torch.optim.Adam(self.transformer.parameters(), lr=self.lr)
-            self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, self.lr_step, gamma=self.lr_decay)
+            # self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, self.lr_step, gamma=self.lr_decay)
+            self.scheduler = torch.optim.lr_scheduler.OneCycleLR(self.optim,max_lr=self.lr,epochs=self.total_epochs,steps_per_epoch=self.len_loader,pct_start=0.3,anneal_strategy='cos',cycle_momentum=False)
             if self.use_scaler:
                 self.scaler = torch.amp.GradScaler()
             else:
@@ -253,7 +259,8 @@ class TransformerBoujee(Model):
         self.transformer.out_lang_embeddings.load_state_dict(state_dicts["outlang_embed"])
         self.optim = torch.optim.Adam(self.transformer.parameters(), lr = self.lr)
         self.optim.load_state_dict(state_dicts["optim"])
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, self.lr_step, gamma=self.lr_decay, last_epoch=state_dicts["epoch"])
+        self.scheduler = torch.optim.lr_scheduler.OneCycleLR(self.optim,max_lr=self.lr,epochs=self.total_epochs,steps_per_epoch=self.len_loader,pct_start=0.3,anneal_strategy='cos',cycle_momentum=False, last_epoch=state_dicts["epoch"])
+        # self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, self.lr_step, gamma=self.lr_decay, last_epoch=state_dicts["epoch"])
         if self.use_scaler:
             self.scaler = torch.amp.GradScaler()
             self.scaler.load_state_dict(state_dicts["scaler"])
@@ -295,3 +302,5 @@ class TransformerBoujee(Model):
             else:
                 new_state_dict[key] = value
         return new_state_dict
+
+

main.py
@@ -7,23 +7,26 @@ import torch
 import torch.nn as nn
 from .Search.Search import greedy_search, beam_search
 from .Log import Logger
+from .Dataset.Language import Embeddings
 
 def eng_to_ger():
-    run_id = 'tm2zle1r'
-    Logger.init_logger(wandb=False, print=True, run_id=run_id)
+    run_id = 'ww8nd8id'
+    Logger.init_logger(wandb=True, print=True, run_id=run_id)
     dataset = EngToGer(byte_pair=True)
-    loss = nn.CrossEntropyLoss(ignore_index=0)
+    print(Embeddings.special_toks_val["<SOS>"])
+    loss = nn.CrossEntropyLoss(ignore_index=Embeddings.special_toks_val["<PAD>"])
     lr_decay_step = 15
-    batch_size = 256
+    batch_size = 128
     lr = 2e-4 * (256 / batch_size)
     lr_decay = .1
     epoch_start = 0
-    epoch_end = lr_decay_step * 3
-    model = TransformerBoujee(lr, lr_decay, lr_decay_step, batch_size)
+    # epoch_end = lr_decay_step * 3
+    epoch_end = 10
+    model = TransformerBoujee(lr, lr_decay, lr_decay_step, batch_size, epoch_end - epoch_start + 1)
     metrics = []
     # fname = "./translate/attempts/ger_mk1/model-epoch-5.pth"
     fname = None
-    train_model(model, dataset, loss, epoch_start, epoch_end, fname,"./translate/attempts/ger_mk1", metrics)
+    train_model(model, dataset, loss, epoch_start, epoch_end, fname,"./translate/attempts/ger_mk2", metrics)
     search = beam_search
     # test_model(model, dataset,loss, "./translate/attempts/french.pth", search,metrics)
     # infer(model, dataset,"./translate/attempts/mk1.pth",search )
@@ -35,7 +38,8 @@ def eng_to_fr():
     Logger.init_logger(wandb=False, print=True, run_id=run_id)
     
     dataset = EngToFr()
-    loss = nn.CrossEntropyLoss(ignore_index=0)
+    print(Embeddings.special_toks_val["<PAD>"])
+    loss = nn.CrossEntropyLoss(ignore_index=Embeddings.special_toks_val["<PAD>"])
     lr_decay_step = 7
     lr = 2e-4
     lr_decay = .1